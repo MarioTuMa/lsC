eb2aecf0d0c2dcb38d6d32b27701d8362a4752df
Marioisdabest <mtutuncumacias1@gmail.com>
2019-11-15 15:37:46 -0500

    moderate progress but doesnt work right now

--- main.c
+++ main.c
@@ -5,25 +5,43 @@
 #include <string.h>
 #include <time.h>
 #include <sys/types.h>
-int main(){
-  DIR* x = opendir(".");
+void printfiles(char * dir, int offset){
+  DIR* x = opendir(dir);
   struct dirent * file = readdir(x);
-  int haveFinishedDirs = 0;
   int totalSize=0;
-  printf("Dirs: \n");
+
   while(file){
     struct stat filestats;
     stat(file->d_name,&filestats);
-    if(0==haveFinishedDirs && filestats.st_size!=4096){
-      printf("Normal Files: \n");
-      haveFinishedDirs=1;
+    if(file->d_type==4){
+      int i;
+      for(i=0;i<offset;i++){
+        printf(" ");
+      }
+      printf("dr %s \n",file->d_name);
+      printfiles(file->d_name,offset+1);
+      totalSize+=filestats.st_size;
     }
-    printf("%s %ldB\n",file->d_name,filestats.st_size);
-    totalSize+=filestats.st_size;
     file = readdir(x);
   }
+  x = opendir(".");
+  printf("Normal Files: \n");
+  file = readdir(x);
+  while(file){
+    struct stat filestats;
+    stat(file->d_name,&filestats);
+    if(file->d_type!=4){
+      printf("nf %s %ldB\n",file->d_name,filestats.st_size);
+      totalSize+=filestats.st_size;
+    }
+    file = readdir(x);
+  }
+  printf("Summary Statistics \nTotal Dir Size %d \n",totalSize);
   //opendir returns a pointer to a directory stream (DIR *) this is a struct
   //readdir returns gives the next entry in the list
 
+}
+int main(){
+  printfiles(".",0);
   return 0;
 }
