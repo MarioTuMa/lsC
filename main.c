0250d5cbee1afa07fd788336d4913bb2a46c3264
Marioisdabest <mtutuncumacias1@gmail.com>
2019-11-17 10:42:41 -0500

    Added recursive file displaying

--- main.c
+++ main.c
@@ -5,38 +5,53 @@
 #include <string.h>
 #include <time.h>
 #include <sys/types.h>
-void printfiles(char * dir, int offset){
+
+int printfiles(char * dir, int offset){
   DIR* x = opendir(dir);
   struct dirent * file = readdir(x);
   int totalSize=0;
-
+  int mySize = 0;
   while(file){
     struct stat filestats;
     stat(file->d_name,&filestats);
     if(file->d_type==4 && strcmp(file->d_name,".")!=0 && strcmp(file->d_name,"..")!=0){
       int i;
       for(i=0;i<offset;i++){
-        printf(" ");
+        printf("\t");
       }
       printf("dr %s \n",file->d_name);
-      printfiles(file->d_name,offset+1);
+      char x[strlen(dir)+strlen(file->d_name)+1];
+      strcpy(x,dir);
+      strcat(x,"/");
+      strcat(x,file->d_name);
+      //printf("%s\n",x);
+      totalSize+=printfiles(x,offset+1);
       totalSize+=filestats.st_size;
+      mySize+=filestats.st_size;
     }
     file = readdir(x);
   }
   x = opendir(".");
-  printf("Normal Files: \n");
+  //printf("Normal Files: \n");
   file = readdir(x);
   while(file){
     struct stat filestats;
     stat(file->d_name,&filestats);
     if(file->d_type!=4){
+      int i;
+      for(i=0;i<offset;i++){
+        printf("\t");
+      }
       printf("nf %s %lldB\n",file->d_name,filestats.st_size);
       totalSize+=filestats.st_size;
+      mySize+=filestats.st_size;
     }
     file = readdir(x);
   }
-  printf("Summary Statistics \nTotal Dir Size %d \n",totalSize);
+  if(offset==0){
+    printf("Summary Statistics \nTotal Dir Size %dB \nTotal Dir Size including subdirs %dB \n",mySize,totalSize);
+  }
+  return totalSize;
   //opendir returns a pointer to a directory stream (DIR *) this is a struct
   //readdir returns gives the next entry in the list
 
